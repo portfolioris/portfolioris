ba2d779d6be9c79f62afeef9f0de07d67234861b
Hulsbosch <j.hulsbosch@colours.nl>
2018-05-20 20:17:12 +0200

    copy, init

--- components/atoms/Icon/stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Icon from './';
-
-storiesOf('atoms.Icon', module).add('Default icon', () => (
-  <Icon />
-));
--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  Config: {
-    apiUrl: 'http://wordpress.local:8888/',
-  },
-};
-
-module.exports = config;
